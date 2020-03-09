fee6d77bb975bb241c964be8d8d2fcaabfa1d84c
sun-kyung <esun1801@naver.com>
2020-03-09 16:40:09 +0900

    20200309

--- bitcamp-java/src/main/java/com/eomcs/mybatis/ex02/Exam0130.java
+++ bitcamp-java/src/main/java/com/eomcs/mybatis/ex02/Exam0130.java
@@ -3,7 +3,6 @@ package com.eomcs.mybatis.ex02;
 
 import java.io.InputStream;
 import java.util.List;
-
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
@@ -12,36 +11,27 @@ import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 public class Exam0130 {
 
   public static void main(String[] args) throws Exception {
-    InputStream inputStream = Resources.getResourceAsStream(
-        "step25/ex6/mybatis-config03.xml");
-    SqlSessionFactory factory = 
+    InputStream inputStream = Resources.getResourceAsStream(//
+        "com/eomcs/mybatis/ex02/mybatis-config03.xml");
+    SqlSessionFactory factory = //
         new SqlSessionFactoryBuilder().build(inputStream);
 
     SqlSession sqlSession = factory.openSession();
 
-    List<Board> list = 
-        sqlSession.selectList("BoardMapper.selectBoard");
+    List<Board> list = sqlSession.selectList("BoardMapper.selectBoard");
 
     for (Board board : list) {
-      System.out.printf("%d, %s, %s, %s\n", 
-          board.getNo(), 
-          board.getTitle(), 
-          board.getContent(),
-          board.getRegisteredDate());
+      System.out.printf("%d, %s, %s, %s, %d\n", //
+          board.getNo(), //
+          board.getTitle(), //
+          board.getContent(), //
+          board.getRegisteredDate(), //
+          board.getViewCount());
     }
 
     sqlSession.close();
   }
 
 }
-
-
-
-
-
-
-
-
-
 
 
--- bitcamp-java/src/main/java/com/eomcs/mybatis/ex02/Exam0220.java
+++ bitcamp-java/src/main/java/com/eomcs/mybatis/ex02/Exam0220.java
@@ -1,10 +1,8 @@
-// Mybatis - SQL에 파라미터 지정하기 : ${}
+// Mybatis - SQL에 파라미터 지정하기 : #{} 의 한계
 package com.eomcs.mybatis.ex02;
 
 import java.io.InputStream;
-import java.util.HashMap;
 import java.util.List;
-
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
@@ -13,26 +11,29 @@ import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 public class Exam0220 {
 
   public static void main(String[] args) throws Exception {
-    InputStream inputStream = Resources.getResourceAsStream(
-        "step25/ex6/mybatis-config05.xml");
-    SqlSessionFactory factory = 
+    InputStream inputStream = Resources.getResourceAsStream(//
+        "com/eomcs/mybatis/ex02/mybatis-config05.xml");
+    SqlSessionFactory factory = //
         new SqlSessionFactoryBuilder().build(inputStream);
 
     SqlSession sqlSession = factory.openSession();
 
-    // mybatis에 SQL문을 만들어 전달할 수 있다.
-    // => SQL 삽입 공격에 노출되기 때문에 이 방식을 사용하지 말라!
-    HashMap<String,Object> paramMap = new HashMap<>();
-    paramMap.put("limitSQL", "limit 9, 5"); 
-
-    List<Board> list = 
-        sqlSession.selectList("BoardMapper.selectBoard", paramMap);
+    // 예) 파라미터로 컬럼 이름을 넘겨주면
+    // 해당 컬럼의 값을 오름차순으로 정렬한다.
+    List<Board> list = sqlSession.selectList(//
+        "BoardMapper.selectBoard1", "title");
+    // => 파라미터 값을 SQL에 그대로 삽입하려면
+    // #{} 문법을 사용해서는 안된다.
+    // ${} 문법을 사용해야 한다.
+    // => #{}은 값을 넣을 때 사용한다.
+    // => 현재 selectBoard1에서는 #{}을 사용했기 때문에
+    // order by가 정상적으로 적용되지 못했다.
 
     for (Board board : list) {
-      System.out.printf("%d, %s, %s, %s\n", 
-          board.getNo(), 
-          board.getTitle(), 
-          board.getContent(),
+      System.out.printf("%d, %s, %s, %s\n", //
+          board.getNo(), //
+          board.getTitle(), //
+          board.getContent(), //
           board.getRegisteredDate());
     }
 
@@ -40,14 +41,5 @@ public class Exam0220 {
   }
 
 }
-
-
-
-
-
-
-
-
-
 
 
